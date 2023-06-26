a7fe36124769b9da38094ec49b6a86e518852b54
John Kulas <jtkulas@ergreports.com>
2023-06-26 14:04:24 -0500

    messing around

--- hexsticker.R
+++ hexsticker.R
@@ -10,19 +10,19 @@ ICCimage2 <- image_read("robin.png")
 
 sticker(subplot=ICCimage1,
         package="ctticc",
-        s_width=1.5,
+        s_width=1.5,                    ## s = image
         s_height=1,
         s_x = 1,
         s_y = 1,
-        p_size = 20,
+        p_size = 20,                    ## p = package name
         p_color = "white",
         p_x = 1,
         p_y = 1.7,
-        h_fill = "#AA3BC2",
+        h_fill = "#AA3BC2",             ## h = border
         h_color = "hotpink",
         h_size = 2.2,
         url="https://github.com/MontclairML/ctticc",
-        u_size = 3,
+        u_size = 3,                     ## u = url
         u_color = "white",
         spotlight = T
 ) %>% print()
@@ -30,19 +30,21 @@ sticker(subplot=ICCimage1,
 
 sticker(subplot=ICCimage2,
         package="ctticc2",
-        s_width=1.5,
+        s_width=1.5,                    ## s = image
         s_height=1,
         s_x = 1,
-        s_y = 1,
-        p_size = 20,
+        s_y = .9,
+        p_size = 20,                    ## p = package name
         p_color = "white",
         p_x = 1,
-        p_y = 1.7,
-        h_fill = "#AA3BC2",
+        p_y = 1.6,
+        h_fill = "#AA3BC2",             ## h = border
         h_color = "hotpink",
         h_size = 2.2,
         url="https://github.com/MontclairML/ctticc",
-        u_size = 3,
+        u_size = 3.5,                     ## u = url
         u_color = "white",
         spotlight = T
 ) %>% print()
+
+
