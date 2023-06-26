edf3d27b167855d3a99e5a96b6d96d1453640dc8
John Kulas <jtkulas@ergreports.com>
2023-06-26 13:57:39 -0500

    playing with hex stickers

---
+++ hexsticker.R
@@ -0,0 +1,48 @@
+## using `hexSticker` to make a sticker - 6/26/23
+
+library(hexSticker)
+library(ctticc)
+library(magick)
+library(tidyverse)
+
+ICCimage1 <- image_read("together.png")
+ICCimage2 <- image_read("robin.png")
+
+sticker(subplot=ICCimage1,
+        package="ctticc",
+        s_width=1.5,
+        s_height=1,
+        s_x = 1,
+        s_y = 1,
+        p_size = 20,
+        p_color = "white",
+        p_x = 1,
+        p_y = 1.7,
+        h_fill = "#AA3BC2",
+        h_color = "hotpink",
+        h_size = 2.2,
+        url="https://github.com/MontclairML/ctticc",
+        u_size = 3,
+        u_color = "white",
+        spotlight = T
+) %>% print()
+
+
+sticker(subplot=ICCimage2,
+        package="ctticc2",
+        s_width=1.5,
+        s_height=1,
+        s_x = 1,
+        s_y = 1,
+        p_size = 20,
+        p_color = "white",
+        p_x = 1,
+        p_y = 1.7,
+        h_fill = "#AA3BC2",
+        h_color = "hotpink",
+        h_size = 2.2,
+        url="https://github.com/MontclairML/ctticc",
+        u_size = 3,
+        u_color = "white",
+        spotlight = T
+) %>% print()
