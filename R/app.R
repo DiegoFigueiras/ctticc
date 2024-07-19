166171f77056b50c06f64dcbbbae252e89e2e891
MontclairML <figueirasd1@montclair.edu>
2024-07-19 14:47:59 -0400

    changing chechbox

---
+++ R/app.R
@@ -0,0 +1,92 @@
+library(psych)
+library(ggplot2)
+library(gridExtra)
+library(tidyverse)
+library(plotly)
+library(shiny)
+
+ctticc <- function(data, items, plot="together", nrow=2, ncol=3) {
+  pseudob <- data.frame(qnorm(colMeans(data, na.rm=TRUE)))*-1
+  ahat <- function(x) {
+    r <- (((2.71828)^x)-(1/(2.71828)^x))/(2.71828-(2.71828)^x)
+    ((((0.51+(0.02*abs(pseudob))+(0.301*pseudob^2))*x)+((0.57-(0.009*abs(pseudob))+(0.19*pseudob^2))*r))*1.71633)
+  }
+
+  alphas <- psych::alpha(data, check.keys = FALSE)
+  citcs <- data.frame(alphas$item.stats$r.drop)
+  pseudoA <- data.frame(ahat(citcs))
+  pseudoB <- -0.000002895614 + (1.535589 * pseudob)
+  df <- as.data.frame(cbind(citcs, pseudoA, pseudoB))
+  colnames(df) <- c("CITC", "PseudoA", "PseudoB")
+  c <- 0
+  df$inum <- row.names(df)
+
+  eq <- function(x, pseudoa, pseudob) {
+    c + ((1-c)*(1/(1+2.71828^(-1.7*(pseudoa*(x-pseudob))))))
+  }
+
+  if (plot == "together") {
+    fun <- function(x, PseudoA, PseudoB) {
+      (1 / (1 + 2.71828^(-1.7 * (PseudoA * (x - PseudoB)))))
+    }
+
+    df_selected <- df[items, ]
+    p <- df_selected %>%
+      crossing(x = seq(-4, 4, .1)) %>%
+      mutate(y = fun(x, PseudoA, PseudoB)) %>%
+      ggplot(aes(x, y, color = inum)) +
+      ylim(0, 1) +
+      geom_line(linewidth = 1.25) +
+      scale_x_continuous(limits = c(-4, 4), labels = c("Low Test Score", "", "Average Test Score", "", "High Test Score")) +
+      labs(y = "p(1.0)", x = "")
+
+    q <- ggplotly(p, tooltip = c("colour"))
+    return(q)
+  }
+
+  return(NULL)
+}
+
+ui <- fluidPage(
+  fluidRow(
+    sidebarPanel(
+      fileInput("file", "Upload CSV File", accept = ".csv"),
+      actionButton("deselect_all", "Deselect All"),
+      checkboxGroupInput("items", "Select Items", choices = NULL, inline=FALSE)
+    ),
+    mainPanel(
+      plotlyOutput('plot1'),
+      p("Make sure your data is structured such that each column is an item in your assessment and each row a respondent. Scores should be binary, 1 and 0."),
+      p("The Item Characteristic Curves are replotted each time you select or de-select an item. User may therefore be interested in gaining visual feedback of item functioning within unique sets of items. When developing subtests this tool should be considered benefitial for making item retention or deletion decisions at the subtest level."),
+
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  data <- reactive({
+    req(input$file)
+    read.csv(input$file$datapath)
+  })
+
+  observe({
+    req(data())
+    updateCheckboxGroupInput(session, "items", choices = colnames(data()), selected = colnames(data()), inline = TRUE)
+  })
+
+  observeEvent(input$deselect_all, {
+    updateCheckboxGroupInput(session, "items", choices = colnames(data()), selected = character(0), inline=TRUE)
+  })
+
+  selectedData <- reactive({
+    req(data())
+    data()[, input$items, drop = FALSE]
+  })
+
+  output$plot1 <- renderPlotly({
+    req(selectedData())
+    ctticc(selectedData(), items = input$items, plot = "together")
+  })
+}
+
+shinyApp(ui = ui, server = server)
